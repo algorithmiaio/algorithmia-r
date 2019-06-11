21eb8fc4a954665629013b4f695b95a017b9d3be
James Sutton <james@algorithmia.com>
2019-06-11 13:16:55 -0300

    fixed RunAllTests

--- tests/RunAllTests.r
+++ tests/RunAllTests.r
@@ -1,7 +1,8 @@
-install.packages("RUnit")
-install.packages("devtools")
-library(devtools)
-install("..")
+# Uncomment these lines to test local (unpublished) changes to the client
+#install.packages("RUnit")
+#install.packages("devtools")
+#library(devtools)
+#install("..")
 library("algorithmia")
 
 library("RUnit")
