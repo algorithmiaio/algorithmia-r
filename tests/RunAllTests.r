ebd4ef42671c2d231a6ae60938bd5cb64a9e00d0
Taylor Raack <traack@algorithmia.io>
2020-09-30 10:01:33 -1000

    CI build runs CRAN submission tests

--- tests/RunAllTests.r
+++ tests/RunAllTests.r
@@ -1,16 +1,20 @@
-# Uncomment these lines to test local (unpublished) changes to the client
-#install.packages("RUnit")
-#install.packages("devtools")
-#library(devtools)
-#install("..")
-library("algorithmia")
+library(RUnit)
 
-library("RUnit")
+pkgname <- "algorithmia"
+require(pkgname, quietly=TRUE, character.only=TRUE) || stop("package '", pkgname, "' not found")
 
-runTestFile("ClientTest.r")
-runTestFile("AlgorithmTest.r")
-runTestFile("DataFileTest.r")
-runTestFile("DataDirectoryTest.r")
-runTestFile("AlgorithmHandlerTest.r")
+message(file.path(path.package(package=pkgname), "../tests"))
 
+# Define RUnit test suite
+suite <- defineTestSuite(name=paste(pkgname, "RUnit Tests"),
+                         dirs = file.path(path.package(package=pkgname), "../tests"),
+                         testFileRegexp=".*Test\\.r$",
+                         testFuncRegexp = "^test.+",
+                         rngKind="default",
+                         rngNormalKind="default")
 
+# Run tests
+result <- runTestSuite(suite)
+
+# Display result tests on the console
+printTextProtocol(result)
---
+++ tools/check-rhub.r
@@ -0,0 +1,72 @@
+chooseCRANmirror(graphics=FALSE, ind=1)
+
+message("Running rhubcheck...")
+
+cr <- rhub::check_for_cran(
+  show_status = TRUE,
+  env_vars = c(`ALGORITHMIA_API_KEY` = "1234")
+)
+
+statuses <- cr[[".__enclos_env__"]][["private"]][["status_"]]
+
+# capture errors, warnings, and notes in data frame
+res <- do.call(rbind, lapply(statuses, function(thisStatus) {
+  data.frame(
+    plaform  = thisStatus[["platform"]][["name"]],
+    errors   = length(thisStatus[["result"]][["errors"]]),
+    warnings = length(thisStatus[["result"]][["warnings"]]),
+    notes    = length(thisStatus[["result"]][["notes"]]),
+    stringsAsFactors = FALSE
+  )
+}))
+
+message("\n\nResults")
+print(res)
+
+message("\n\nGrabbing Rcheck logs")
+logCandidates <- do.call(rbind, lapply(
+  cr$urls()$artifacts,
+  function(thisUrl) {
+    logFile <- tools::file_path_sans_ext(dir("./logs/"))
+    data.frame(
+      url = file.path(
+        thisUrl,
+        paste0(unique(sapply(statuses, `[[`, "package"))[[1L]], ".Rcheck"),
+        c(paste0(logFile, "00check.log"), paste0(logFile, "00check.log"))
+      ),
+      fileName = paste(
+        basename(thisUrl),
+        c(paste0(logFile, "00check.log"), paste0(logFile, "00check.log")),
+        sep = "-"
+      ),
+      stringsAsFactors = FALSE
+    )
+  }
+))
+
+message("Downloading Rcheck logs from rhub: \n")
+logLogs <- apply(
+  logCandidates,
+  1L,
+  function(x) {
+    try(
+      utils::download.file(x["url"], x["fileName"], quiet = TRUE),
+      silent = TRUE
+    )
+  }
+)
+names(logLogs) <- logCandidates[["fileName"]]
+
+message("Attempting do cat log logs from rhub: \n\n")
+invisible(lapply(
+  names(logLogs[logLogs == 0L]),
+  function(x) {
+    message("\n", x)
+    cat(readLines(x), sep = "\n")
+  }
+))
+
+# report error status if errors or warnings exist
+if (any(colSums(res[2L:3L]) > 0)) {
+  stop("Some checks with errors or warnings.")
+}
