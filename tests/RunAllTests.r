5f0808738af93431f7c042cf9735c17f322dd000
john.bragg <John.bragg@ruralsourcing.com>
2020-12-03 13:32:19 -0700

    unit tests pathing fix

--- tests/RunAllTests.r
+++ tests/RunAllTests.r
@@ -3,11 +3,11 @@ library(RUnit)
 pkgname <- "algorithmia"
 require(pkgname, quietly=TRUE, character.only=TRUE) || stop("package '", pkgname, "' not found")
 
-message(file.path(path.package(package=pkgname), "tests"))
+message(file.path(path.package(package=pkgname), "../tests"))
 
 # Define RUnit test suite
 suite <- defineTestSuite(name=paste(pkgname, "RUnit Tests"),
-                         dirs = file.path(path.package(package=pkgname), "tests"),
+                         dirs = file.path(path.package(package=pkgname), "../tests"),
                          testFileRegexp=".*Test\\.r$",
                          testFuncRegexp = "^test.+",
                          rngKind="default",
